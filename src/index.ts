6282da3dd4cfd599d21e506cb37e7ef51fd5a7e6
Jerry <x.jerry.wang@gmail.com>
2026-03-19 03:43:14 +0000

    perf: ignore write if content is the same

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { createUnplugin, type UnpluginInstance } from 'unplugin'
 import type { Options } from './types'
 import { type LoadEnvOptions, loadEnv, toArray } from './core'
 import { generateTypedefCode, generateTypescriptCode } from './core/generator'
-import { writeFile } from 'fs/promises'
-import { join } from 'path'
+import { readFile, writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { existsSync } from 'node:fs'
 
 const plugin: UnpluginInstance<Options | undefined> = createUnplugin((options) => {
   const opt: LoadEnvOptions = {
@@ -36,16 +37,28 @@ const plugin: UnpluginInstance<Options | undefined> = createUnplugin((options) =
 
     const filePath = join(opt.cwd, dtsFile)
 
-    await writeFile(filePath, code)
+    await writeFileIfChanged(filePath, code)
 
     if (options?.envFile) {
       const envFilePath = join(opt.cwd, options.envFile)
 
       const code = generateTypescriptCode(env, opt.prefix)
 
-      await writeFile(envFilePath, code)
+      await writeFileIfChanged(envFilePath, code)
     }
   }
 })
+
+async function writeFileIfChanged(filepath: string, code: string) {
+  let existsCode = ''
+
+  if (existsSync(filepath)) {
+    existsCode = await readFile(filepath, 'utf8')
+  }
+
+  if (existsCode !== code) {
+    await writeFile(filepath, code)
+  }
+}
 
 export default plugin
